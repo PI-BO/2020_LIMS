79a4a4619ac05628c7b217138cc11965d083bdfb
M.Wbr <>
2020-12-08 17:41:21 +0100

    update: IndexServlet mit Weiterleitung, Fragekatalog, add: AfterIndexServlet

--- src/servlets/IndexServlet.java
+++ src/servlets/IndexServlet.java
@@ -3,6 +3,7 @@ package servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,14 +12,23 @@ import javax.servlet.http.HttpServletResponse;
 public class IndexServlet extends HttpServlet{
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
+		// wird nicht ausgeführt da weiter unten der Request direkt an AfterIndexServlet weitergeleitet wurde
 		PrintWriter htmlWriter = response.getWriter();
 		htmlWriter.println("<h1>Willkommen</>");
 		htmlWriter.println("<h2>zum Index Servlet!</>");
+		
+		forwardRequestToRoute(request, response, "AfterIndexServletRoute");
 	}
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+	}
+	
+	private void forwardRequestToRoute(HttpServletRequest request, HttpServletResponse response, String route) throws ServletException, IOException{
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher(route);
+		requestDispatcher.forward(request, response);
 	}
 
 }
