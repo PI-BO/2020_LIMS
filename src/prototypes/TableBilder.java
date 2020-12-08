90a09e40e6632622d5ebe37bcec954260bd17171
Dominic Narwutsch <dominic.narwutsch@gmail.com>
2020-12-08 21:11:54 +0100

    Add: Log4j logger to Project

--- src/prototypes/TableBilder.java
+++ src/prototypes/TableBilder.java
@@ -1,5 +1,8 @@
 package prototypes;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,45 +13,44 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TableBilder {
-	
-	private static String tableName = "bilder";
-	private static String columnName = "bild"; 
-	
-	public static void getFile(File file, Connection connection) throws SQLException, IOException{
-		
-		String sql = "SELECT " + columnName + " FROM " + tableName;
-	    PreparedStatement stmt = connection.prepareStatement(sql);
-	    ResultSet resultSet = stmt.executeQuery();
+    private static final Logger logger = LogManager.getLogger(TableBilder.class.getSimpleName());
+    private static String tableName = "bilder";
+    private static String columnName = "bild";
 
-	    while (resultSet.next()) {
-	    	
-	    	//File image = new File(fileLocation);
-	      
-			 if(file.exists()){
-				 System.out.println("FILE EXISTS!");		//todo: Meldung erstellen falls etwas ueberschrieben werden soll 
-				 return;
-			 }
-			  
-			 FileOutputStream fos = new FileOutputStream(file);
-			
-			 byte[] buffer = new byte[1];
-			 InputStream is = resultSet.getBinaryStream(1);
+    public static void getFile(File file, Connection connection) throws SQLException, IOException {
 
-			 while (is.read(buffer) > 0) {
-			   fos.write(buffer);
-			 }
-			 
-			 fos.close();
-			 
-			 System.out.println("FILE CREATED!");
-	    }
-	}
-	
-	public static String getTableName(){
-		return tableName;
-	}
-	
-	public static String getColumnName(){
-		return columnName;
-	}
+        String sql = "SELECT " + columnName + " FROM " + tableName;
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            ResultSet resultSet = stmt.executeQuery();
+
+            while (resultSet.next()) {
+
+                //File image = new File(fileLocation);
+
+                if (file.exists()) {
+                    logger.debug("FILE EXISTS!"); //todo: Meldung erstellen falls etwas ueberschrieben werden soll
+                    return;
+                }
+
+                try (FileOutputStream fos = new FileOutputStream(file)) {
+                    byte[] buffer = new byte[1];
+                    InputStream is = resultSet.getBinaryStream(1);
+
+                    while (is.read(buffer) > 0) {
+                        fos.write(buffer);
+                    }
+                }
+
+                logger.debug("FILE CREATED!");
+            }
+        }
+    }
+
+    public static String getTableName() {
+        return tableName;
+    }
+
+    public static String getColumnName() {
+        return columnName;
+    }
 }
