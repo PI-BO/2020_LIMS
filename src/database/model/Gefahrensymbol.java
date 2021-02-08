c637b5e23cf7199a3dfc62560fdf1bf91e75ff69
M.Wbr <>
2021-02-08 17:42:53 +0100

    add: Gefahrensymbole, update: Probeneingang

---
+++ src/database/model/Gefahrensymbol.java
@@ -0,0 +1,51 @@
+package database.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import exceptions.ModelNotFoundException;
+
+public class Gefahrensymbol extends Model{
+
+	@Override
+	public String getPrimaryKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPrimaryKeyColumn() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTable() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setAttributes(ResultSet resultSet) throws SQLException, ModelNotFoundException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getValues() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getRelationSchema() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveToDatabase() {
+		// TODO Auto-generated method stub
+		
+	}
+}
