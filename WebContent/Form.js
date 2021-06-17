89598de71884eac5fee16b2abc9da0916d6f57d3
M.Wbr <>
2021-06-17 17:01:02 +0200

    alten Form-code entfernt

--- WebContent/Form.js
+++ WebContent/Form.js
@@ -1,36 +1,5 @@
 const Form = {};
 
-Form.addSubmitAlt = function (url, formId, messageId, callbackOnSuccess) {
-
-	$("#" + formId).submit(function (e) {
-
-		e.preventDefault();
-
-		var submitData = {};
-		const form = document.getElementById(formId);
-
-		for (var i = 0; i < e.target.length; i++) {
-
-			submitData[e.target[i].name] = e.target[i].value;
-		}
-
-		var posting = $.post(url, submitData);
-		posting.done(function (data) {
-
-			if (data["status"] === "error") addErrorMessage(messageId, data["message"]);
-
-			if (data["status"] === "success") {
-
-				setRequiredInputFieldsToGreen(form);
-				addSuccessMessage(messageId, data["message"]);
-				removeSubmitButton(form);
-
-				if (callbackOnSuccess !== undefined) callbackOnSuccess();
-			}
-		});
-	})
-}
-
 Form.addSubmit = function (submitType, url, formId, messageId, callbackOnSuccess) {
 
 	let form = document.getElementById(formId);
